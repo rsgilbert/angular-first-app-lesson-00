bedb876b97b570c0f9a81d298b11c688499151b2
SSG <gssenyonjo@gmail.com>
2023-07-01 18:20:25 +0300

    Displayed a single housing location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
     selector: 'app-home',
@@ -14,11 +15,23 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
     </form>
 </section>
 <section class="results">
-    <app-housing-location></app-housing-location>
+    <app-housing-location
+        [housingLocation]="housingLocation"    
+    >
+    </app-housing-location>
 </section>
   `,
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+    housingLocation: HousingLocation = {
+        id: 1000,
+        name: 'Nsambya Gardens',
+        city: 'Kampala city',
+        state: 'Central Uganda',
+        photo: 'assets/pillars.png',
+        availableUnits: 10,
+        wifi: true,
+        laundry: false
+    }
 }
